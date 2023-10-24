6e746ed14d03c09d4574be129216bb7c46f93f74
e-gugliotti-NOAA <elizabeth.gugliotti@noaa.gov>
2023-10-24 21:31:19 +0000

    update output function

--- R/get_sims_output.r
+++ R/get_sims_output.r
@@ -14,29 +14,45 @@
 get_sims_output <- function(dir = dir, 
                             new_filename = "unnested_iterations"){
   
-sims_models <- list.dirs(file.path(dir, new_filename), recursive = FALSE)
-model_names <- unique(gsub("-.*","",basename(sims_models)))
+  sims_models <- list.dirs(file.path(dir, new_filename), recursive = FALSE)
+  model_names <- unique(gsub("-.*","",basename(sims_models)))
 
-# use furrr with below
-for(m in model_names){
+  ncores <- parallel::detectCores() - 1 
+  future::plan(multisession, workers = ncores)
+
+  sims_results <- furrr::future_map(model_names, ~run_plot_compars(
+    model_names = .x,
+    dir = dir,
+    new_filename = new_filename,
+    sims_models = sims_models
+  ))
+  }
+
+
+run_plot_compars <- function(model_names,
+                             dir,
+                             new_filename,
+                             sims_models
+                             ){
   plot_path <- file.path(dir, new_filename, paste0(model_names[1],"_plots"))
   if(! plot_path %in% sims_models){
     dir.create(path = plot_path)
   }
   model_match <- grep(model_names[1], sims_models, value = TRUE)
+  model_match <-model_match[!grepl("plots", model_match)]
   sims_output <- SSgetoutput(dirvec = model_match)
   sims_summary <- SSsummarize(sims_output)
   
-  SSplotComparisons(sims_summary, plot = FALSE, print = TRUE, plotdir = plot_path)
-  }
-
-
-
-# need to plot comparisons per test model
-# make plots comparing the 6 models
-SSplotComparisons(sims_summary,
-  endyrvec = endyrvec,
-  legendlabels = paste("Data", 0:-5, "years")
-) 
-
+  model_labels <- gsub(paste0(model_names[1],"-"),"", basename(model_match))
+  model_labels <- gsub("iteration","i", model_labels)
+  sims_summary$modelnames <- model_labels
+  
+  SSplotComparisons(sims_summary, plot = FALSE, print = TRUE, 
+                    plotdir = plot_path,
+                    legendlabels = model_labels)
 }
+
+
+
+
+
